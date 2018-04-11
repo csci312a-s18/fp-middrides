82aba0b941cace49d355eb96d20866c8af9b026d
jvolz <jvolz@middlebury.edu>
2018-04-11 19:19:53 -0400

    start of introductory user screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+
+const MiddRidesTitle = styled.h1`
+ text-align: center;
+
+`
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      viewmode: 'UserStart',
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      if(this.state.viewmode === "UserStart"){
+      return (
+        <div className="App">
+          <MiddRidesTitle> MIDDRIDES </MiddRidesTitle>
+        </div>
+        //Button
+      );
+      }
+      else if(this.state.viewmode === "RequestRide"){
+        return ("hello world");
+        //show request form
+      }
   }
 }
 
