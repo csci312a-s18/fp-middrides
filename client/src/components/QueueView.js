15fc318c1b33152bcf915adc4fa3677e79bc98a8
Crystal <cpaudyal@middlebury.edu>
2018-04-12 20:55:31 -0400

    fully functional queue-view

--- client/src/components/QueueView.js
+++ client/src/components/QueueView.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const HorizontalUL = styled.ul`
-  list-style:none;
+const Table = styled.table`
+  border: 1px solid black;
+  border-collapse: collapse;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
-// to-do:
-// work on the alignment
+const Td = styled.td`
+  border: 1px solid black;
+  height: 50px;
+  vertical-align: bottom;
+  padding: 15px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+`;
 
-const ViewHeader = styled.li`
-  display:inline;
-  padding: 20px;
-  font-weight: bold;
+const Th = styled.th`
+  border: 1px solid black;
+  height: 50px;
+  padding: 15px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+  background-color: #4CAF50;
+  color: white;
 `;
 
 const requests = [];
@@ -21,29 +35,42 @@ const req1 = {
   from: 'Bihall',
   to: 'Atwater',
   count: 2,
+  completed: 'No',
 };
 
 const req2 = {
-  id: 1,
+  id: 2,
   from: 'Proctor',
   to: 'ADK',
   count: 3,
+  completed: 'Yes',
+};
+
+const req3 = {
+  id: 3,
+  from: 'E lot',
+  to: 'Ridgeline',
+  count: 1,
+  completed: 'No',
 };
 
 requests.push(req1);
 requests.push(req2);
+requests.push(req3);
 
-function QueueViewHeader() {
-  const items = ['Id', 'Origin', 'Destination', '#people'];
-  const sectionItems = items.map(section => (
-    <ViewHeader key={section}>{section}</ViewHeader>
-  ));
-  return (
-    <div>
-      <HorizontalUL>{sectionItems}</HorizontalUL>
-    </div>
-  );
-}
+// function QueueViewHeader() {
+//   const items = ['Id', 'Origin', 'Destination', '#people'];
+//   const sectionItems = items.map(section => (
+//     <ViewHeader key={section}>{section}</ViewHeader>
+//   ));
+//   return (
+//     <div>
+//       <HorizontalUL>{sectionItems}</HorizontalUL>
+//     </div>
+//   );
+// }
+
+const headers = ['Id', 'Origin', 'Destination', '#people', 'Completed?'];
 
 class QueueView extends Component {
   constructor() {
@@ -54,20 +81,45 @@ class QueueView extends Component {
   }
 
   render() {
-    const queueHeader = (<QueueViewHeader />);
-    const requestList = this.state.queue.map(section => (
-      <ViewHeader key={section}>
-        {section.id} {section.from} {section.to} {section.count}
-        <br />
-      </ViewHeader>
-    ));
     return (
-      <div>
-        {queueHeader}
-        <HorizontalUL>{requestList}</HorizontalUL>
-      </div>
+      <Table>
+        <thead>
+          <tr>
+            {headers.map(title =>
+              <Th key={title}>{title}</Th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.queue.map(request =>
+            <tr key={request.id}>
+              <Td>{request.id}</Td>
+              <Td>{request.from}</Td>
+              <Td>{request.to}</Td>
+              <Td>{request.count}</Td>
+              <Td>{request.completed}</Td>
+            </tr>)}
+        </tbody>
+      </Table>
     );
   }
 }
+
+  // render() {
+  //
+  //   const queueHeader = (<QueueViewHeader />);
+  //   const requestList = this.state.queue.map(section => (
+  //     <ViewHeader key={section}>
+  //       {section.id}     {section.from}     {section.to}     {section.count}
+  //       <br />
+  //     </ViewHeader>
+  //   ));
+  //   return (
+  //     <div>
+  //       {queueHeader}
+  //       <HorizontalUL>{requestList}</HorizontalUL>
+  //     </div>
+  //   );
+  // }
+// }
 
 export default QueueView;
