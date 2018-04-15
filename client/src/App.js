a12f67fc49a40046034f8bc8162347aa15059204
rachel <rkang@middlebury.edu>
2018-04-15 14:21:18 -0400

    Fixed errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import ContentArea from './components/ContentArea';
-import Form from './components/Form';
+//import Form from './components/Form';
 
 const MiddRidesTitle = styled.p`
  text-align: center;
--- client/src/components/UserView.js
+++ client/src/components/UserView.js
@@ -1,7 +1,5 @@
 import React, { } from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
-
 
 function UserView(props) {
   const { changeView } = props;
