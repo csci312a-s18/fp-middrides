bd66b76f7c9be72a63e3960e3b3722088907e919
reidbuzby <rbuzby@middlebury.edu>
2018-04-15 16:03:44 -0400

    Pushing current status on MongoDB server. Server currently works when running from command line, but currently unable to run the server from JavaScript files in project. Build will currently fail.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID, MongoError } = require('mongodb'); // eslint-disable-line no-unused-vars
+
+const app = express();
+
+const corsOptions = {
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Accept', 'X-Requested-With', 'Origin'],
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get('/requests', (request, response, next) => {
+  db.collection('requests').find().toArray().then((documents) => { // eslint-disable-line no-unused-vars
+    response.send(documents);
+  }, next);
+});
+
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
   render() {
@@ -19,4 +41,7 @@ class App extends Component {
   }
 }
 
-export default App;
+module.exports = {
+  app,
+  setDb: (newDb) => { db = newDb; }, // eslint-disable-line no-undef
+};
