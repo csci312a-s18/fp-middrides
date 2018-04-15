18f03f06257a576641a7eebf326882136d53bf4b
reidbuzby <rbuzby@middlebury.edu>
2018-04-15 16:03:44 -0400

    Pushing current status on MongoDB server. Server currently works when running from command line, but currently unable to run the server from JavaScript files in project. Build will currently fail.

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,28 @@ const MiddRidesTitle = styled.p`
  font-size: 2em;
 `;
 
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID, MongoError } = require('mongodb'); // eslint-disable-line no-unused-vars
+
+const app = express();
+
+const corsOptions = {
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Accept', 'X-Requested-With', 'Origin'],
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get('/requests', (request, response, next) => {
+  db.collection('requests').find().toArray().then((documents) => { // eslint-disable-line no-unused-vars
+    response.send(documents);
+  }, next);
+});
+
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
   render() {
@@ -22,4 +44,7 @@ class App extends Component {
   }
 }
 
-export default App;
+module.exports = {
+  app,
+  setDb: (newDb) => { db = newDb; }, // eslint-disable-line no-undef
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
+/* eslint-disable no-console */
 const http = require('http');
-const { app } = require('./server');
+const url = require('url');
+const { MongoClient } = require('mongodb');
+const { app, setDb } = require('./app');
 
-const server = http.createServer(app).listen(process.env.PORT || 3001);
-console.log('Listening on port %d', server.address().port); // eslint-disable-line no-console
+const mongoURL = process.env.MONGODB_URI || 'https://gentle-sea-19680.herokuapp.com/';
+
+MongoClient.connect(mongoURL, (err, database) => {
+  if (err) {
+    console.error(err);
+  } else {
+    // Don't start server unless we have successfully connect to the database
+    const db = database.db(url.parse(mongoURL).pathname.slice(1)); // Extract database name
+    setDb(db);
+
+    // We create the server explicitly (instead of using app.listen()) to
+    // provide an example of how we would create a https server
+    const server = http.createServer(app).listen(process.env.PORT || 3000);
+    console.log('Listening on port %d', server.address().port);
+  }
+});
