e5787e3fa5ec687032367f4965bd34307a027f58
Crystal <cpaudyal@middlebury.edu>
2018-04-16 15:05:26 -0400

    fixed lint errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Form from './components/Form';
+// import Form from './components/Form';
 import styled from 'styled-components';
 import ContentArea from './components/ContentArea';
 // import Form from './components/Form';
