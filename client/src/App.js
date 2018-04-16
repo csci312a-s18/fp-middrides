5e26b60babf921a8f631d26d67b528fa59f4be2e
reidbuzby <rbuzby@middlebury.edu>
2018-04-16 15:04:24 -0400

    Fixed MongoDB connection to Heroku application. Currently able to preform curl requests on server.

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const cors = require('cors');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { ObjectID, MongoError } = require('mongodb'); // eslint-disable-line no-unused-vars
-
-const app = express();
-
-const corsOptions = {
-  methods: ['GET', 'PUT', 'POST', 'DELETE'],
-  origin: '*',
-  allowedHeaders: ['Content-Type', 'Accept', 'X-Requested-With', 'Origin'],
-};
-
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-
-app.get('/requests', (request, response, next) => {
-  db.collection('requests').find().toArray().then((documents) => { // eslint-disable-line no-unused-vars
-    response.send(documents);
-  }, next);
-});
-
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
   render() {
@@ -41,7 +19,4 @@ class App extends Component {
   }
 }
 
-module.exports = {
-  app,
-  setDb: (newDb) => { db = newDb; }, // eslint-disable-line no-undef
-};
+export default App;
