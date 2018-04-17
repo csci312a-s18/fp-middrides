aa6ae9e8df44a62be5b5f0d57dd42286bd6ee1a2
Woojin Richardson <woojin.richardson@gmail.com>
2018-04-17 00:39:16 -0400

    Integrate RequestForm into ContentArea

--- client/src/components/ContentArea.js
+++ client/src/components/ContentArea.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import QueueView from './QueueView';import RequestForm from './RequestForm';
+import QueueView from './QueueView';
+import RequestForm from './RequestForm';
 
 const DivContainer = styled.div`
   width: 80%;
   margin-left: auto;
   margin-right: auto;
+`;
+
+const ButtonBar = styled.div`
 `;
 
 class ContentArea extends Component {
@@ -17,32 +21,47 @@ class ContentArea extends Component {
       requests: [],
       currentRequest: null,
     };
+
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/requests')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status_text);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ requests: data });
+      })
+      .catch(err => console.log(err)); // eslint-disable-line no-console
   }
 
   handleFormReturn(newRequest) {
       if (newRequest) { // Not a cancel
-
-        // if (this.state.currentArticle) { // Update existing article
-        //   fetch(`/articles/${this.state.currentArticle._id}`, {
-        //     method: 'PUT',
-        //     body: JSON.stringify(newArticle),
-        //     headers: new Headers({ 'Content-type': 'application/json' }),
-        //   }).then((response) => {
-        //     if (!response.ok) {
-        //       throw new Error(response.status_text);
-        //     }
-        //     return response.json();
-        //   }).then((updatedArticle) => {
-        //     const updatedCollection = this.state.collection.map((article) => {
-        //       if (article._id === updatedArticle._id) {
-        //         return updatedArticle;
-        //       }
-        //       return article;
-        //     });
-        //     this.setState({ collection: updatedCollection });
-        //     this.setState({ currentArticle: updatedArticle });
-        //   }).catch(err => console.log(err)); // eslint-disable-line no-console
-        // } else { // Create new article
+        if (this.state.currentArticle) { // Update existing request
+          fetch(`/requests/${this.state.currentRequest._id}`, {
+            method: 'PUT',
+            body: JSON.stringify(newRequest),
+            headers: new Headers({ 'Content-type': 'application/json' }),
+          }).then((response) => {
+            if (!response.ok) {
+              throw new Error(response.status_text);
+            }
+            return response.json();
+          }).then((updatedRequest) => {
+            const updatedRequests = this.state.requests.map((request) => {
+              if (request._id === updatedRequest._id) {
+                return updatedRequest;
+              }
+              return request;
+            });
+            this.setState({ requests: updatedRequests });
+            this.setState({ currentRequest: updatedRequest });
+          }).catch(err => console.log(err)); // eslint-disable-line no-console
+        } else { // Create new request
           fetch('/requests', {
             method: 'POST',
             body: JSON.stringify(newRequest),
@@ -56,13 +75,28 @@ class ContentArea extends Component {
             const updatedRequests = this.state.requests;
             updatedRequests.push(createdRequest);
             this.setState({ requests: updatedRequests });
-            // this.setState({ currentArticle: createdArticle });
+            this.setState({ currentArticle: createdRequest });
           }).catch(err => console.log(err)); // eslint-disable-line no-console
         }
-
-      // Switch to the user main view
-      this.setState({ viewmode: 'UserStart'});
     }
+    // Switch to the user main view
+    this.setState({ viewmode: 'UserStart'});
+  }
+
+  handleCancel() {
+    const cancelledRequest = Object.assign(this.state.currentRequest, { active: false });
+    fetch(`/requests/${this.state.currentRequest._id}`, {
+      method: 'PUT',
+      body: JSON.stringify(cancelledRequest),
+      headers: new Headers({ 'Content-type': 'application/json' }),
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status_text);
+      }
+      return response.json();
+    }).catch(err => console.log(err)); // eslint-disable-line no-console
+    this.setState({ currentRequest: null });
+  }
 
   render() {
     if (this.state.viewmode === 'UserStart') {
@@ -75,22 +109,31 @@ class ContentArea extends Component {
       const queueview = (<QueueView
         requests={this.state.requests}
       />);
-      const btnRequestRide = (<input
-        type="button"
-        value="Request Ride"
-        onClick={() => this.setState({ viewmode: 'RequestRide' })}
+
+      const requestRideButton = <input type="button" value="Request Ride" onClick={() => this.setState({ viewmode: 'RequestRide' })} />;
+      const changeRideButton = <input type="button" value="Change Ride" onClick={() => this.setState({ mode: 'RequestRide' })} />;
+      const cancelRideButton = (<input type="button" value="Cancel Ride" onClick={this.handleCancel}
       />);
+
+      let buttons;
+      if (this.state.currentArticle) {
+        buttons = (<ButtonBar>{changeRideButton} {cancelRideButton}</ButtonBar>);
+      } else {
+        buttons = (<ButtonBar>{requestRideButton}</ButtonBar>);
+      }
+
       return (
         <DivContainer>
           {gps}
           {queueview}
           <br />
-          {btnRequestRide}
+          {buttons}
         </DivContainer>
       );
     }
     return (
       <RequestForm
+        request={this.state.currentRequest}
         complete={(newRequest) => {this.handleFormReturn(newRequest); }}
       />
     );
