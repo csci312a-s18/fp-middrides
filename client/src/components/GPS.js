70991b8a658de6306aeb785cacd8fe0d348e42d9
jvolz <jvolz@middlebury.edu>
2018-04-24 13:35:42 -0400

    linted

--- client/src/components/GPS.js
+++ client/src/components/GPS.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+/* eslint-disable import/no-named-as-default */
 import MapContainer from './Map';
 
 
@@ -6,19 +7,20 @@ class GPS extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat:44.0153,
-      lng:-73.1673,
+      lat: 44.0153,
+      lng: -73.1673,
     };
   }
 
-  render(){
-    return(
-    <MapContainer lat = {this.state.lat} lng = {this.state.lng}/>
-  );}
+  render() {
+    return (
+      <MapContainer lat={this.state.lat} lng={this.state.lng} />
+    );
+  }
 }
 export default GPS;
 
-    /*
+/*
     // return (
     //   <Map />
     // )
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
-
-import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import PropTypes from 'prop-types';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 const style = {
   width: '100%',
-  height: '400px'
-}
+  height: '400px',
+};
 
 export class MapContainer extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       lat: props.lat ? props.lat : 44.0153,
       lng: props.lng ? props.lng : -73.1673,
-    }
+    };
   }
   render() {
     return (
-      <Map google={this.props.google}
-          initialCenter={{
-            lat:this.state.lat, lng:this.state.lng
+      <Map
+        google={this.props.google}
+        initialCenter={{
+            lat: this.state.lat, lng: this.state.lng,
           }}
-          style={style}
-          zoom={14}>
-      <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
+        style={style}
+        zoom={14}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name="Current location"
+        />
 
       </Map>
     );
@@ -32,5 +36,11 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyCsls1CEvSm07LGXVVt1xoTKeYY1dagA8k')
-})(MapContainer)
+  apiKey: ('AIzaSyCsls1CEvSm07LGXVVt1xoTKeYY1dagA8k'),
+})(MapContainer);
+
+MapContainer.propTypes = {
+  lat: PropTypes.string.isRequired,
+  lng: PropTypes.string.isRequired,
+  google: PropTypes.string.isRequired,
+};
