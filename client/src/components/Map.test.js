0cb80dda36581b27b1d4dee316fc9d7d1ae85e8a
jvolz <jvolz@middlebury.edu>
2018-04-26 13:49:23 -0400

    linted

--- client/src/components/Map.test.js
+++ client/src/components/Map.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import MapContainer from './Map';
+import MapContainer from './Map';// eslint-disable-line import/no-named-as-default
 
 describe('MapContainer', () => {
-  const fakeLat = 46.0153;
-  const fakeLng = -77.1673;
+  // const fakeLat = 46.0153;
+  // const fakeLng = -77.1673;
   test('Component renders', () => {
     const wrapper = shallow(<MapContainer complete={jest.fn} />);
     expect(wrapper.exists()).toBe(true);
