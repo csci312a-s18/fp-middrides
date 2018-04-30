6c48b76d0aad83d82d8b155b678d046ee503f283
Woojin Richardson <woojin.richardson@gmail.com>
2018-04-30 16:53:53 -0400

    Add consideration for number of passengers in each request when enumerating possible paths

--- client/src/components/GPS.js
+++ client/src/components/GPS.js
@@ -1,59 +1,90 @@
 let paths = [];
 
 let request1 = {
+  passengers: 10,
   currentLocation: 'A',
   destination: 'B',
   isPickedUp: true
 };
 
 let request2 = {
+  passengers: 6,
   currentLocation: 'C',
   destination: 'D',
   isPickedUp: false
 };
 
-let requests = [request1, request2];
+let request3 = {
+  passengers: 5,
+  currentLocation: 'E',
+  destination: 'F',
+  isPickedUp: false
+};
 
-function recursiveAlgorithm(currentStop, requests, path) {
+let request4 = {
+  passengers: 5,
+  currentLocation: 'A',
+  destination: 'D',
+  isPickedUp: false
+};
+
+let requests = [request1, request2, request3, request4];
+
+function recursiveAlgorithm(currentStop, requests, path, seatsLeft) {
+
+  let updatedRequests = [];
+  requests.forEach(request => updatedRequests.push(Object.assign({}, request)));
 
   // either set request to "picked up" or remove from list
-  requests.forEach(request => {
+  updatedRequests.forEach(request => {
     if (!request.isPickedUp && request.currentLocation === currentStop) {
+      seatsLeft -= request.passengers;
       request.isPickedUp = true;
     }
     else if (request.isPickedUp && request.destination === currentStop) {
-      requests.splice(requests.indexOf(request), 1); // request has been "dropped off"
+      seatsLeft += request.passengers;
+      updatedRequests.splice(updatedRequests.indexOf(request), 1); // request has been "dropped off"
     }
   });
 
   path.push(currentStop);
+  console.log('---------------------------------');
+  console.log(seatsLeft);
 
   // create list of available stops
   let available = [];
-  for (let i = 0; i < requests.length; i++) {
-    if (!requests[i].isPickedUp) {
-      available.push(requests[i].currentLocation);
+  for (let i = 0; i < updatedRequests.length; i++) {
+    if (!updatedRequests[i].isPickedUp) {
+      if (seatsLeft - updatedRequests[i].passengers >= 0){
+        available.push(updatedRequests[i].currentLocation);
+      }
     }
     else {
-      available.push(requests[i].destination);
+      available.push(updatedRequests[i].destination);
     }
-  }
 
+  }
+  //console.info(updatedRequests);
+  console.log('Path: ' + path);
   console.log(available);
+
 
   // base case
   if (available.length === 0) {
-    console.log('Path: ' + path);
+    //console.log('Path: ' + path);
     paths.push(path);
   }
-
+  
   // recursive step
   else {
+
     for (let i = 0; i < available.length; i++) {
-      recursiveAlgorithm(available[i], requests.slice(), path.slice());
+      // double check if we need to copy seatsLeft before passing to the next funciton
+      recursiveAlgorithm(available[i], updatedRequests, path.slice(), seatsLeft);
     }
   }
 }
 
-recursiveAlgorithm('A', requests, []);
+recursiveAlgorithm('A', requests, [], 4);
 console.log(paths);
+//console.log(paths.length);
