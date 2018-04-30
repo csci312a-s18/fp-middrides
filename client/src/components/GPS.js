352382e24710276c228f9ac5505cdb06526b3284
silas Keeter <silas.keeter@gmail.com>
2018-04-30 15:47:54 -0400

    Major architecture overhaul for GPS and Map components. GPS component now only handles pushing shuttle location to Mongo. Pulling the location now only occurs in the Map component. Marker with live location of shuttle currently working. Need to add a dispatcher boolean prop for next commit.

--- client/src/components/GPS.js
+++ client/src/components/GPS.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 /* eslint-disable import/no-named-as-default */
 import MapContainer from './Map';
-//import geolocation from 'geolocation';
+// import geolocation from 'geolocation';
 
 const options = {
   enableHighAccuracy: false,
@@ -9,78 +9,57 @@ const options = {
   maximumAge: 0,
 };
 
+const latLngID = '5ae396ed734d1d133182d27a';
+function updateLocation(latitude, longitude) {
+  const newLocation = Object.assign({}, {
+    _id: {
+      $oid: latLngID,
+    },
+    latitude: null,
+    longitude: null,
+  }, { latitude, longitude });
+
+  fetch(`/shuttleLocation/${latLngID}`, {
+    method: 'PUT',
+    body: JSON.stringify(newLocation),
+    headers: new Headers({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status_text);
+    }
+    return response.json();
+  }).catch(err => console.log(err)); // eslint-disable-line no-console
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function success(points) {
+  updateLocation(points.coords.latitude, points.coords.longitude);
+}
+
 class GPS extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      lat: null,
-      lng: null,
-      latLngID: '5ae396ed734d1d133182d27a',
-    };
+    this.success = this.success.bind(this);
   }
 
   componentDidMount() {
+    // change true to boolean for whether dispatcher account is open or not
     if (true) {
-      setTimeout(() => {
-        navigator.geolocation.getCurrentPosition(this.success, this.error, options);
+      setInterval(() => {
+        navigator.geolocation.getCurrentPosition(success, error, options);
       }, 1000);
     }
-    else {
-      this.getLocation();
-    }
-  }
-
-  success(points) {
-    console.log(points.coords.latitude, points.coords.longitude);
-    this.updateLocation(points.coords.latitude, points.coords.longitude);
-    this.setState( { lat: points.coords.latitude, lng: points.coords.longitude });
-  }
-
-  error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
-
-  updateLocation(latitude, longitude) {
-    const newLocation = Object.assign({}, {
-      "_id": {
-          "$oid": this.state.latLngID
-      },
-      "latitude": null,
-      "longitude": null
-    }, { "latitude": latitude, "longitude": longitude });
-
-    fetch(`/shuttleLocation/${this.state.latLngID}`, {
-      method: 'PUT',
-      body: JSON.stringify(newLocation),
-      headers: new Headers({
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      }),
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status_text);
-      }
-      return response.json();
-    }).catch(err => console.log(err)); // eslint-disable-line no-console
-  }
-
-  getLocation() {
-    fetch(`/shuttleLocation`, { headers: new Headers({ Accept: 'application/json' }) })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status_text);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({ lat: data[0].latitude, lng: data[0].longitude });
-      })
-      .catch(err => console.log(err)); // eslint-disable-line no-console
   }
 
   render() {
     return (
-      <MapContainer lat={this.state.lat} lng={this.state.lng} />
+      <MapContainer />
     );
   }
 }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-import styled from 'styled-components';
 
 const style = {
   width: '80%',
   height: '400px',
 };
 
+
+const centerStart = { lat: 44.0153, lng: -73.1673 };
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: props.lat,
-      lng: props.lng,
+      lat: 0,
+      lng: 0,
     };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.getLocation();
+    }, 1000);
+  }
+
+  getLocation() {
+    fetch('/shuttleLocation', { headers: new Headers({ Accept: 'application/json' }) })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status_text);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ lat: data[0].latitude, lng: data[0].longitude });
+      })
+      .catch(err => console.log(err)); // eslint-disable-line no-console
   }
 
   render() {
@@ -22,16 +43,15 @@ export class MapContainer extends Component {
       <Map
         google={this.props.google}
         initialCenter={{
-            lat: this.state.lat, lng: this.state.lng,
+            lat: centerStart.lat, lng: centerStart.lng,
           }}
         style={style}
         zoom={14}
       >
         <Marker
-          onClick={this.onMarkerClick}
           name="Current location"
+          position={{ lat: this.state.lat, lng: this.state.lng }}
         />
-
       </Map>
     );
   }
@@ -42,7 +62,5 @@ export default GoogleApiWrapper({
 })(MapContainer);
 
 MapContainer.propTypes = {
-  lat: PropTypes.number.isRequired,
-  lng: PropTypes.number.isRequired,
   google: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
