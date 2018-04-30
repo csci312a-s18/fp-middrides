d48872a0e7962c96c0d49cb89326f8a967ee631f
Woojin Richardson <woojin.richardson@gmail.com>
2018-04-30 17:00:40 -0400

    Improve algorithm
    -Handle duplicates
    -Return objects with _id

--- client/src/components/GPS.js
+++ client/src/components/GPS.js
@@ -1,83 +1,90 @@
-let paths = [];
+const paths = [];
 
-let request1 = {
-  passengers: 10,
+const request1 = {
+  _id: 1,
+  passengers: 7,
   currentLocation: 'A',
   destination: 'B',
-  isPickedUp: true
+  isPickedUp: false,
 };
 
-let request2 = {
-  passengers: 6,
-  currentLocation: 'C',
-  destination: 'D',
-  isPickedUp: false
-};
-
-let request3 = {
-  passengers: 5,
-  currentLocation: 'E',
-  destination: 'F',
-  isPickedUp: false
-};
-
-let request4 = {
-  passengers: 5,
+const request2 = {
+  _id: 2,
+  passengers: 2,
   currentLocation: 'A',
   destination: 'D',
-  isPickedUp: false
+  isPickedUp: false,
 };
 
-let requests = [request1, request2, request3, request4];
+const request3 = {
+  _id: 3,
+  passengers: 3,
+  currentLocation: 'A',
+  destination: 'F',
+  isPickedUp: false,
+};
+
+const request4 = {
+  _id: 4,
+  passengers: 2,
+  currentLocation: 'A',
+  destination: 'D',
+  isPickedUp: false,
+};
+
+const requests = [request1, request2, request3];
 
 function recursiveAlgorithm(currentStop, requests, path, seatsLeft) {
-
-  let updatedRequests = [];
+  const updatedRequests = [];
+  const id = [];
   requests.forEach(request => updatedRequests.push(Object.assign({}, request)));
 
+  // when multiple requests are made from the same stop so that the bus is full, this algorithm gives priority to the requests that were made earlier
+
   // either set request to "picked up" or remove from list
-  updatedRequests.forEach(request => {
-    if (!request.isPickedUp && request.currentLocation === currentStop) {
+  updatedRequests.forEach((request) => {
+    if (!request.isPickedUp && request.currentLocation === currentStop && request.passengers <= seatsLeft) {
       seatsLeft -= request.passengers;
       request.isPickedUp = true;
-    }
-    else if (request.isPickedUp && request.destination === currentStop) {
+      id.push(request._id);
+    } else if (request.isPickedUp && request.destination === currentStop) {
       seatsLeft += request.passengers;
+      id.push(request._id);
       updatedRequests.splice(updatedRequests.indexOf(request), 1); // request has been "dropped off"
     }
   });
 
-  path.push(currentStop);
+
+  path.push({ currentStop, id });
   console.log('---------------------------------');
-  console.log(seatsLeft);
+  // console.log(seatsLeft);
 
   // create list of available stops
-  let available = [];
+  const available = [];
   for (let i = 0; i < updatedRequests.length; i++) {
     if (!updatedRequests[i].isPickedUp) {
-      if (seatsLeft - updatedRequests[i].passengers >= 0){
+      if (seatsLeft - updatedRequests[i].passengers >= 0) {
         available.push(updatedRequests[i].currentLocation);
       }
-    }
-    else {
+    } else {
       available.push(updatedRequests[i].destination);
     }
-
   }
-  //console.info(updatedRequests);
-  console.log('Path: ' + path);
-  console.log(available);
+  // console.info(updatedRequests);
+  console.log(`Path: ${path}`);
+  console.log(`currentStop: ${currentStop}`);
+  console.log(`id: ${id}`);
+  console.log(`available: ${available}`);
 
 
   // base case
   if (available.length === 0) {
-    //console.log('Path: ' + path);
+    // console.log('Path: ' + path);
     paths.push(path);
   }
-  
+
   // recursive step
   else {
-
     for (let i = 0; i < available.length; i++) {
       // double check if we need to copy seatsLeft before passing to the next funciton
       recursiveAlgorithm(available[i], updatedRequests, path.slice(), seatsLeft);
@@ -85,6 +92,12 @@ function recursiveAlgorithm(currentStop, requests, path, seatsLeft) {
   }
 }
 
-recursiveAlgorithm('A', requests, [], 4);
+recursiveAlgorithm('A', requests, [], 14);
 console.log(paths);
-//console.log(paths.length);
+for (let i = 0; i < paths.length; i++) {
+  for (let j = 0; j < paths[i].length; j++) {
+    console.log(`ids: ${paths[i][j].id}`);
+  }
+}
+// paths.forEach(request => console.log(request.id));
+// console.log(paths.length);
