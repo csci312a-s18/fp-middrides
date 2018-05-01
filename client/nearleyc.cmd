5c05106bc8e1d58a599f0008294f89771cfdff61
Ian <iblow@middlebury.edu>
2018-04-30 21:59:16 -0400

    Revert "This is the linted version of the dispatcher mode changes" reverse push commit This reverts commit 402e966f0ea3a63400b80d11a46819c51fb7ffb3.
    :wq
    :

--- client/nearleyc.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\nearley\bin\nearleyc.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\nearley\bin\nearleyc.js" %*
-)
--- server/seek-table.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\seek-bzip\bin\seek-bzip-table" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\seek-bzip\bin\seek-bzip-table" %*
-)
